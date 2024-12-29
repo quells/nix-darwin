df0b5a53efc90d45235f0722c93f8fdcff36e482
Kai Wells <support@kaiwells.me>
2024-12-28 22:10:40 -0800

    devbox commit

---
+++ dotfiles/.bin/ghoti.sh
@@ -0,0 +1,6 @@
+export PATH="/Users/kwells/.local/share/devbox/global/default/.devbox/nix/profile/default/bin:${PATH}"
+export PATH="/Users/kwells/.nix-profile/bin:${PATH}"
+export PATH="/nix/var/nix/profiles/default/bin:${PATH}"
+export PATH="/usr/local/bin:${PATH}"
+export SHELL='fish'
+exec fish
