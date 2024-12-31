f7a746f21881d43ec228bd02b14b41b87e789dd9
Kai Wells <support@kaiwells.me>
2024-12-31 10:33:08 -0800

    devbox commit

--- dotfiles/.bin/ghoti.sh
+++ dotfiles/.bin/ghoti.sh
@@ -2,5 +2,6 @@ export PATH="/Users/kwells/.local/share/devbox/global/default/.devbox/nix/profil
 export PATH="/Users/kwells/.nix-profile/bin:${PATH}"
 export PATH="/nix/var/nix/profiles/default/bin:${PATH}"
 export PATH="/usr/local/bin:${PATH}"
+export PATH="/opt/podman/bin:${PATH}"
 export SHELL='fish'
 exec fish
