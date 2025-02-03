bb8d96f80583276d4c6a33bc838c3e06f5103e19
Kai Wells <support@kaiwells.me>
2025-02-02 22:26:14 -0800

    devbox commit

--- dotfiles/.config/fish/functions/myip.fish
+++ dotfiles/.config/fish/functions/myip.fish
@@ -1,4 +1,15 @@
 function myip --description 'Current external IP address'
-    set -l RESP (curl --silent --location -H 'Accept: application/json' https://api.kaiwells.me/myip; or '{"ip": "disconnected"}')
+    argparse 4/ipv4 6/ipv6 -- $argv; or return
+
+    set -l URL 'https://myip-proud-surf-2427.fly.dev/'
+    set CURL_CMD "curl --silent -H 'Accept: application/json' $URL"
+    if set -ql _flag_ipv4
+        set CURL_CMD "curl -g -4 --silent -H 'Accept: application/json' $URL"
+    end
+    if set -ql _flag_ipv6
+        set CURL_CMD "curl -g -6 --silent -H 'Accept: application/json' $URL"
+    end
+
+    set -l RESP (eval "$CURL_CMD"; or '{"ip": "disconnected"}')
     echo $RESP | jq -r '.ip'
 end
